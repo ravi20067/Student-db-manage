2c409f6c843c4e01068b845b7b355b3d7fed744f
Ravi Kumar <kumarr3829@gmail.com>
2025-09-18 15:39:49 +0530

    5451

--- src/main/java/com/first/project/Controller/StudentPageController.java
+++ src/main/java/com/first/project/Controller/StudentPageController.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +39,7 @@ public class StudentPageController {
         return "redirect:/";
     }
 
-    @DeleteMapping("/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteStudent(@PathVariable int id) {
         repo.deleteById(id);
         return "redirect:/";
